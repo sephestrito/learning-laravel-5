2bbccd9c0ad6237d638f2919278041a785eec638
Joseph Estrito <seph.s32@gmail.com>
2015-11-11 23:09:06 +0800

    JCE.DEV

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5</div>
+                <div class="title">JCE.DEV</div>
             </div>
         </div>
     </body>
